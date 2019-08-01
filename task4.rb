e145e9fc698f2494a5418960ab5e2c6268097eae
Valentin Markin <jobedano@gmail.com>
2019-08-02 03:32:50 +0400

    task 4

---
+++ task4.rb
@@ -0,0 +1,9 @@
+vowels_hash = Hash.new()
+latin = (:A..:Z).to_a 
+vowels_arr = [:A, :E, :I, :O, :U]
+
+latin.each.with_index do |letter, i|
+  vowels_hash.store(letter, i+1) if vowels_arr.include?(letter)
+end
+
+puts vowels_hash
